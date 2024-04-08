1fdada19f74d462e9ee2a01009da62e8d00e5f01
Mohammadreza Amani <More.amani@yahoo.com>
2024-04-08 14:30:16 +0330

    update footer style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,48 +1,86 @@
-import GitHubIcon from "@mui/icons-material/GitHub";
+import { PiCodesandboxLogoThin, PiLinkedinLogoThin } from "react-icons/pi";
+import { VscGithubAlt } from "react-icons/vsc";
+import { IoMdContact } from "react-icons/io";
+import { FaUniversity } from "react-icons/fa";
+import { FaPhoenixFramework } from "react-icons/fa6";
+import { SiReaddotcv } from "react-icons/si";
 import { siteConfig } from "../configs/site";
 import { commonArgs } from "../configs/types";
+import Wave from "react-wavify";
+// import { Tooltip } from "@mui/material";
 
 function Footer({ theme, lang, slug }: commonArgs) {
   let findLang = siteConfig.langs.find((item) => item.lang === lang);
   if (!findLang) {
     findLang = siteConfig.langs[0];
   }
+  const buttonSize =
+    "h-10 sm:h-12 w-10 sm:w-12 m-2 bg-blue-700 rounded-full p-2 ";
   return (
     <div className="w-full">
-      <footer className={`text-center p-4 ${theme.white} ${theme.secondary}`}>
-        <p className="text-sm">
-          {findLang.author} © {new Date().getFullYear()}
-        </p>
-        <p className="text-sm">
-          {findLang.author} is open-source on{" "}
-          <a
-            href={siteConfig.github}
-            target="_blank"
-            rel="noreferrer"
-            className="text-white"
-          >
-            <GitHubIcon />
-          </a>
-        </p>
+      <Wave
+        fill="#1ED4DE"
+        className="mt-10 border-gray-400"
+        // paused={false}
+        style={{ display: "flex" }}
+        options={{
+          height: 70,
+          amplitude: 20,
+          speed: 0.13,
+          points: 3,
+        }}
+      ></Wave>{" "}
+      <footer className={`text-center ${theme.white} ${theme.secondary}`}>
+        <Wave
+          fill="rgb(17 24 39)"
+          className="-mt-12 border-gray-400"
+          // paused={false}
+          style={{ display: "flex" }}
+          options={{
+            height: 10,
+            amplitude: 20,
+            speed: 0.1,
+            points: 3,
+          }}
+        ></Wave>
+        <div className="flex justify-center px-1">
+          <PiLinkedinLogoThin className={` bg-blue-700 ${buttonSize}`} />
+          <VscGithubAlt className={` bg-gray-700 ${buttonSize}`} />
+          <IoMdContact className={` bg-green-700 ${buttonSize}`} />
+          <FaUniversity className={` bg-yellow-700 ${buttonSize}`} />
+          <FaPhoenixFramework className={` bg-red-700 ${buttonSize}`} />
+          <SiReaddotcv className={` bg-purple-700 ${buttonSize}`} />
+          {/* <div className="flex justify-center mt-12">
+            <Tooltip title={"projects"}>
+              <a href="/#/project">
+                <PiCodesandboxLogoThin className="h-12 w-12 m-2 bg-yellow-500 rounded-full p-2 text-white" />
+              </a>
+            </Tooltip>
+            <Tooltip title={"about"}>
+              <a href="/#/about">
+                <IoMdContact className="h-12 w-12 m-2 bg-green-700 rounded-full p-2 text-white" />
+              </a>
+            </Tooltip>
+            <Tooltip title={"education"}>
+              <a href="/#/education">
+                <FaUniversity className="h-12 w-12 m-2 bg-yellow-700 rounded-full p-2 text-white" />
+              </a>
+            </Tooltip>
+            <Tooltip title={"blog"}>
+              <a href="/#/blog">
+                <FaPhoenixFramework className="h-12 w-12 m-2 bg-red-700 rounded-full p-2 text-white" />
+              </a>
+            </Tooltip>
+            <Tooltip title={"resume"}>
+              <a href="/#/resume">
+                <SiReaddotcv className="h-12 w-12 m-2 bg-purple-700 rounded-full p-2 text-white" />
+              </a>
+            </Tooltip>
+          </div> */}
+        </div>
       </footer>
     </div>
   );
 }
 
 export default Footer;
-
-// import { PiLinkedinLogoThin } from "react-icons/pi";
-// import { VscGithubAlt } from "react-icons/vsc";
-// import { IoMdContact } from "react-icons/io";
-// import { FaUniversity } from "react-icons/fa";
-// import { FaPhoenixFramework } from "react-icons/fa6";
-// import { SiReaddotcv } from "react-icons/si";
-// <div className="flex justify-center mt-12">
-// <PiLinkedinLogoThin className="h-12 w-12 m-2 bg-blue-700 rounded-full p-2 text-white" />
-// <VscGithubAlt className="h-12 w-12 m-2 bg-gray-700 rounded-full p-2 text-white" />
-// <IoMdContact className="h-12 w-12 m-2 bg-green-700 rounded-full p-2 text-white" />
-// <FaUniversity className="h-12 w-12 m-2 bg-yellow-700 rounded-full p-2 text-white" />
-// <FaPhoenixFramework className="h-12 w-12 m-2 bg-red-700 rounded-full p-2 text-white" />
-// <SiReaddotcv className="h-12 w-12 m-2 bg-purple-700 rounded-full p-2 text-white" />
-
-// </div>
