2afca3fa7343c7091795d25d9a1ebe0e47621a3b
Mohammadreza Amani <More.amani@yahoo.com>
2024-04-08 11:25:55 +0330

    minor changes

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border bg-card text-card-foreground",
       className
     )}
     {...props}
@@ -33,6 +33,7 @@ const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
+  // eslint-disable-next-line jsx-a11y/heading-has-content
   <h3
     ref={ref}
     className={cn("font-semibold leading-none tracking-tight", className)}
