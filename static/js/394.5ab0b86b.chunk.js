bd32b7504c88cd64f1780eeed5745501f06c14a7
Mohammadreza Amani <More.Amani@yahoo.com>
2024-04-11 07:01:36 +0330

    Updates

--- static/js/394.5ab0b86b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmr_template=self.webpackChunkmr_template||[]).push([[394],{5394:(t,e,n)=>{n.d(e,{Xnq:()=>i});var r=n(3441);function i(t){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"m112 160-64 64 64 64"},child:[]},{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M64 224h294c58.76 0 106 49.33 106 108v20"},child:[]}]})(t)}}}]);
-//# sourceMappingURL=394.5ab0b86b.chunk.js.map
